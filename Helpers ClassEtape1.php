ae22385bd00baaca87da72c9e0a48ff55a26e211
Goudmant <yvangoudmant@gmail.com>
2020-12-08 15:33:32 +0100

    maj4

--- Helpers ClassEtape1.php
+++ Helpers ClassEtape1.php
@@ -6,25 +6,28 @@
     <title>Document</title>
 </head>
 <body>
-    <!--<form action="<?php echo $action; ?>" method="POST">
+<?php
+    <form action="<?php echo $action; ?>" method="POST">
         <input name="nom" value="<?php echo $nom ?>" >
         <input name="prenom" value="<?php echo $prenom ?>" >
         <input type="submit" value="Modifier" >
-    </form>-->
+    </form>
 
-    <!--echo $form = new Form();
+    echo $form = new Form();
     echo $form->create($action); // créer le début du formulaire
     echo $form->text('nom',$nom); // créer un input de type texte avec comme valeur par défaut $nom
     echo $form->text('prenom',$prenom); // créer un input de type texte avec comme valeur par défaut $prenom
     echo $form->submit('Modifier'); //Créer un bouton pour soumettre le formulaire se nommant Modifier
-    echo $form->end(); //fermer le formulaire-->
+    echo $form->end(); //fermer le formulaire
 
-    <!--Le début d'un formulaire <form ...>
+    Le début d'un formulaire <form ...>
     Un input text <input type="text"...>
     Un select <select ...> ...</select>
     Un bouton submit <button type="submit"></button>
     Un textarea <textarea ...> ...</textarea>
     Un radio button <input type="radio"...>
-    Une checkbox <input type="checkbox">-->
+    Une checkbox <input type="checkbox">
+
+    ?>
 </body>
 </html>
--- htdocs/POO.php
+++ htdocs/POO.php
@@ -1,9 +1,15 @@
 <?php
 
-class becodekeller {
-        private $firstname;
-        public $lastname; 
-        protected $age;
+interface transpiration 
+{
+    public function transpirer();
+}
+
+class becodekeller implements transpiration
+{
+    private $firstname;
+    public $lastname; 
+    protected $age;
 
     public function __CONSTRUCT ($lastname, $firstname, $age)
     {
@@ -13,21 +19,26 @@ class becodekeller {
 
     }
 
-    public function setAge ($age)
+    public function transpirer () 
+    {
+        return "je suis un junior et je sue !";
+    }
+
+    public function setAge ($age) // Permet de fermer l'age a un nombres entier entre 18 et 99 
     {
         if (is_int($age) && $age >= 18 && $age <= 99) {
         $this->age = $age; 
-        } else {
-            throw new Exception ("l'age d'un junior n'est pas un entier entre 18 et 99 !");
+        } else { // affiche un message d'erreur si la function n'est pas correct
+            throw new Exception ("l'age d'un Becodeur n'est pas un entier entre 18 et 99 !");
         }
     }
 
-    public function getAge()
+    public function getAge() // va appeler la function age proteger 
     {
         return $this->age;
     }
 
-    public function presentation ()
+    public function presentation () // structure des membres
     {
         var_dump ("Bonjour, je suis Web dev junior à Becode Charleroi je m'appelle $this->firstname , $this->lastname et j'ai $this->age ");
     }
@@ -35,47 +46,59 @@ class becodekeller {
 
 class coach  extends becodekeller {
 
-    public $classe;
+    private $classe;
 
-public function __CONSTRUCT ($lastname, $firstname, $age, $classe)
-{
-    parent::__construct ($lastname, $firstname, $age);
+    public function __CONSTRUCT ($lastname, $firstname, $age, $classe)
+    {
+        parent::__construct ($lastname, $firstname, $age);
 
-    $this->classe = $classe;
+        $this->classe = $classe;
+
+    }
+    public function presentation () // structure Coachs
+    {
+        var_dump ("Hellooooooo,Moi $this->lastname, $this->firstname , coach de la classe Web dev junior, je m'engage a faire réussir les $this->classe à Becode Charleroi. Au passage je n'ai que {$this->getAge()} ans! ");
+    }
 
 }
-public function presentation ()
-{
-    var_dump ("Hellooooooo,Moi $this->lastname, $this->firstname , coach de la classe Web dev junior, je m'engage a faire réussir les $this->classe à Becode Charleroi. Au passage je n'ai que {$this->getAge()} ans! ");
-}
-}
 
-$coach -> presentation();
-$coach -> classe ();
+    fairetranspirer($coach);
+    fairetranspirer($becodekeller);
 
-$coach1 = new coach ("maitredieusenseimaster", "Pierre", 99, "Keller");
+    function fairetranspirer (transpirer $objet) {
+        var_dump("Je vais vous faire suer avec l'auto-apprentisage :{$objet->transpirer()}");
+    }
 
-$becodekeller -> presentation ();
-
+// Les membres 
 $becodekeller1 = new junior ("Koala","Dorian", 37);
 $becodekeller2 = new junior ("Poulpe","Yvan", 31);
-$becodekeller3 = new junior ("ecureuil","Julien", 22);
-$becodekeller4 = new junior ("chinchilla","Joann", 34);
-$becodekeller5 = new junior ("lemurien","Abderrahman", 32);
-$becodekeller6 = new junior ("kangourou","Thomas", 28);
-$becodekeller7 = new junior ("grenouille","Yannick", 29);
-$becodekeller8 = new junior ("rougegorge","Florence", 34);
-$becodekeller9 = new junior ("taupe","Antoine", 29);
-$becodekeller10 = new junior ("suricate","Olivia", 22);
-$becodekeller11 = new junior ("girafe","Gaeatano", 55);
-$becodekeller12 = new junior ("rhinocéros","Philippe", 26);
-$becodekeller13 = new junior ("tigre","Pauline", 27);
-$becodekeller14 = new junior ("hibou","Frederic", 29);
-$becodekeller15 = new junior ("lion","Nathanael", 25);
-$becodekeller16 = new junior ("ours","Kutligin", 28);
-$becodekeller17 = new junior ("puma","Jonathan", 35);
-$becodekeller18 = new junior ("renard","Henrique", 30);
-$becodekeller19 = new junior ("pingouin","Manon", 22);
-$becodekeller20 = new junior ("chameau","Valentin", 28);
+$becodekeller3 = new junior ("Ecureuil","Julien", 22);
+$becodekeller4 = new junior ("Chinchilla","Joann", 34);
+$becodekeller5 = new junior ("Lemurien","Abderrahman", 32);
+$becodekeller6 = new junior ("Kangourou","Thomas", 28);
+$becodekeller7 = new junior ("Grenouille","Yannick", 29);
+$becodekeller8 = new junior ("Rougegorge","Florence", 34);
+$becodekeller9 = new junior ("Taupe","Antoine", 29);
+$becodekeller10 = new junior ("Suricate","Olivia", 22);
+$becodekeller11 = new junior ("Girafe","Gaeatano", 55);
+$becodekeller12 = new junior ("Rhinocéros","Philippe", 26);
+$becodekeller13 = new junior ("Tigre","Pauline", 27);
+$becodekeller14 = new junior ("Hibou","Frederic", 29);
+$becodekeller15 = new junior ("Lion","Nathanael", 25);
+$becodekeller16 = new junior ("Ours","Kutligin", 28);
+$becodekeller17 = new junior ("Puma","Jonathan", 35);
+$becodekeller18 = new junior ("Renard","Henrique", 30);
+$becodekeller19 = new junior ("Pingouin","Manon", 22);
+$becodekeller20 = new junior ("Chameau","Valentin", 28);
+
+// Partie visible Membres voir var_dump
+$becodekeller -> presentation ();
+
+// les Coachs 
+$coach1 = new coach ("maitredieusenseimaster", "Pierre", 99, "Keller");
+
+// Partie visible Coachs voir var_dump
+$coach -> presentation();
+
 
 ?>
