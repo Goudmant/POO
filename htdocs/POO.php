b5762b749e1f992cdd5cde7f81f1476fa6b64953
Goudmant <yvangoudmant@gmail.com>
2020-12-08 10:17:15 +0100

    maj2

--- htdocs/POO.php
+++ htdocs/POO.php
@@ -1,3 +1,43 @@
 <?php
 
+class becodekeller {
+        private $firstname;
+        private $lastname; 
+        private $age;
+
+    public function __CONSTRUCT ($firstname, $lastname, $age)
+    {
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->age = $age;
+
+    }
+
+    public function presentation ()
+    {
+        var_dump ("Bonjour, je suis Web dev junior à Becode Charleroi je m'appelle $this->firstname , $this->lastname et j'ai $this->age ");
+    }
+}
+
+$becodekeller1 = new junior ("","", );
+$becodekeller2 = new junior ("","", );
+$becodekeller3 = new junior ("","", );
+$becodekeller4 = new junior ("","", );
+$becodekeller5 = new junior ("","", );
+$becodekeller6 = new junior ("","", );
+$becodekeller7 = new junior ("","", );
+$becodekeller8 = new junior ("","", );
+$becodekeller9 = new junior ("","", );
+$becodekeller10 = new junior ("","", );
+$becodekeller11 = new junior ("","", );
+$becodekeller12 = new junior ("","", );
+$becodekeller13 = new junior ("","", );
+$becodekeller14 = new junior ("","", );
+$becodekeller15 = new junior ("","", );
+$becodekeller16 = new junior ("","", );
+$becodekeller17 = new junior ("","", );
+$becodekeller18 = new junior ("","", );
+$becodekeller19 = new junior ("","", );
+$becodekeller20 = new junior ("","", );
+
 ?>
